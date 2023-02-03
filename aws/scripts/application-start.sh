9d573af697c0f30c8f8f394c688c3b31667371b8
Jason Tam <jasontytam@gmail.com>
2023-02-03 12:17:18 +0000

    script start 5

--- aws/scripts/application-start.sh
+++ aws/scripts/application-start.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-#set -xe
+set -xe
 
 . "/root/.bashrc"
-whoami
-node --version
-npm --version
-env
-
 
 cd /usr/local/Netflix-React/
-serve -s build > log/Netflix-React.$(date '+%Y%m%d_%H%M%S').log
+serve -s build > log/Netflix-React.$(date '+%Y%m%d_%H%M%S').log &
